158f0af7f903a03de5da034f38bce87db7248315
youssefMoJo <youssefrafaat@yahoo.com>
2023-09-04 20:24:25 -0230

    lower case the early access section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -14,8 +14,7 @@ const Footer = () => {
       <div className="footerTexts">
         <h1 className="text1">Did we catch your attention ?</h1>
         <p className="text2">
-          We plan to start testing soon, type your email address to be updated
-          with our progress
+          We'll begin testing soon. Enter your email to get <span style={{ color: "#22ED98" }}>early access</span> to the app.
         </p>
         <h2 className="text3">Don’t worry, we won’t spam</h2>
         <Email />
